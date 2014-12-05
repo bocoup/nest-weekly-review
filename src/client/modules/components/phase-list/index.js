7069c590b1a55acb336d2e27af5a75a2291a0219
Mike Pennisi <mike@mikepennisi.com>
2014-12-05 11:39:21 -0500

    Implement phase listing navigation

--- src/client/modules/components/phase-list/index.js
+++ src/client/modules/components/phase-list/index.js
@@ -1,7 +1,8 @@
 'use strict';
 var Ractive = require('ractive/ractive.runtime');
 
-var DAY_MS = 24 * 60 * 60 * 1000;
+var weekNumber = require('../../util/week-num');
+var WEEK_MS = 7 * 24 * 60 * 60 * 1000;
 
 module.exports = Ractive.extend({
   template: require('./template.html'),
@@ -9,18 +10,37 @@ module.exports = Ractive.extend({
     firstWeek: new Date(),
     numWeeks: 0
   },
+  formatPhase: function(date) {
+    return {
+      date: date,
+      url: '/year/' + date.getFullYear() + '/week/' + 3 + '/phase/' + 4 + '/',
+    };
+  },
+  reviewUrl: function(date) {
+    return '/year/' + date.getFullYear() + '/week/' + weekNumber.fromDate(date) + '/';
+  },
   computed: {
-    weekRange: function() {
+    nextWeek: function() {
+      var date = new Date(this.get('firstWeek').getTime() + WEEK_MS);
+      return this.reviewUrl(date);
+    },
+    prevWeek: function() {
+      var date = new Date(this.get('firstWeek').getTime() - WEEK_MS);
+      return this.reviewUrl(date);
+    },
+    phases: function() {
       var first = this.get('firstWeek');
       var num = this.get('numWeeks');
-      var weeks = [first];
+      var phases = [];
       var idx;
 
-      for (idx = 1; idx < num; ++idx) {
-        weeks.push(new Date(first.getTime() + idx * 7 * DAY_MS));
+      for (idx = 0; idx < num; ++idx) {
+        phases.push(
+          this.formatPhase(new Date(first.getTime() + idx * WEEK_MS))
+        );
       }
 
-      return weeks;
+      return phases;
     }
   }
 });

c7683e635536da565eb624c185c4323c56ab8a46
Mike Pennisi <mike@mikepennisi.com>
2014-12-10 10:34:38 -0500

    Define minimal style for application layout

--- src/client/modules/components/layout/index.js
+++ src/client/modules/components/layout/index.js
@@ -5,6 +5,7 @@ var Review = require('../review/index');
 
 module.exports = Ractive.extend({
   template: require('./template.html'),
+  css: require('./style.css'),
   components: {
     'bp-phase-table': PhaseTable,
     'bp-review': Review

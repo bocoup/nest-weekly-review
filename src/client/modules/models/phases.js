6aaa9769ca6a41b8b1d746bc845c13f64262cdf6
Mike Pennisi <mike@mikepennisi.com>
2014-12-23 11:50:31 -0500

    Remove unused method

--- src/client/modules/models/phases.js
+++ src/client/modules/models/phases.js
@@ -3,7 +3,6 @@ var Collection = require('ampersand-rest-collection');
 
 var Phase = require('./phase');
 var Employees = require('./employees');
-var getOrFetch = Collection.prototype.getOrFetch;
 
 var API_ROOT = require('../api-root');
 
@@ -13,29 +12,6 @@ module.exports = Collection.extend({
 
   initialize: function() {
     this.employees = new Employees();
-  },
-
-  getOrFetchold: function() {
-    var employees = this.employees;
-    var callback = arguments[arguments.length - 1];
-    var args = Array.prototype.slice.call(arguments);
-
-    args[arguments.length - 1] = function(err, phase) {
-      var done = callback.bind(this, err, phase);
-      if (err) {
-        done();
-        return;
-      }
-
-      employees.fetchBetween({
-        start: phase.get('date_start'),
-        end: phase.get('date_end'),
-        success: done,
-        error: callback
-      });
-    };
-
-    getOrFetch.apply(this, args);
   },
 
   fetchByProject: function(projectId, options) {

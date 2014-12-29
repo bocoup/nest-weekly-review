a95004b9e921ce783e9c935a52f63c1ae5acba70
Mike Pennisi <mike@mikepennisi.com>
2014-12-29 13:54:38 -0500

    Display review information in phase overview

--- src/client/modules/components/phase-table-row/index.js
+++ src/client/modules/components/phase-table-row/index.js
@@ -26,15 +26,28 @@ module.exports = Component.extend({
       var viewWidth = this.get('data-num-weeks');
       var viewStart = this.get('data-first-week');
       var phaseStart = this.get('phase.first_day');
+      var phaseTimestamp = phaseStart.getTime();
       var between = Math.round(weekNumber.between(phaseStart, viewStart));
       var phaseLength = this.get('phase.calendar_weeks');
       var weeks = [];
-      var weekOffset, idx;
+      var weekOffset, idx, isActive, review;
 
       for (idx = 0; idx < viewWidth; ++idx) {
         weekOffset = between + idx;
+        isActive = weekOffset >= 0 && weekOffset < phaseLength;
+        review = null;
+
+        // As an optimization, only look up reviews for weeks on which the
+        // phase is active.
+        if (isActive) {
+          review = this.get('phase').reviews.forDate(
+            phaseTimestamp + weekOffset * WEEK_MS
+          );
+        }
+
         weeks.push({
-          isActive: weekOffset >= 0 && weekOffset < phaseLength,
+          isActive: isActive,
+          review: review,
           reviewUrl: this.reviewUrl(weekOffset)
         });
       }
